620a748731c574026bfe1ed08fbe885a3a84b221
Wenjun Li <berryfl@gmail.com>
2023-03-18 15:54:13 +0800

    add instance_tab sql definition

---
+++ sqls/init.sql
@@ -0,0 +1,12 @@
+CREATE TABLE instance_tab (
+    id bigserial not null PRIMARY KEY,
+    name varchar(64) not null,
+    enable_http bool not null,
+    enable_https bool not null,
+    cert_name varchar(64) not null,
+    updated_by varchar(64) not null,
+    created_at int not null,
+    updated_at int not null,
+    deleted_at int not null,
+    UNIQUE (name)
+);
